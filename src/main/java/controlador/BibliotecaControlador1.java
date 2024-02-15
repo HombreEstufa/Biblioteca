e176d5412a5a433687a1ecbe30f569850fc8394e
Roberto <robertodelbao12@gmail.com>
2024-02-15 17:01:40 +0100

    JDBC

--- src/main/java/controlador/BibliotecaControlador1.java
+++ src/main/java/controlador/BibliotecaControlador1.java
@@ -6,12 +6,16 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import modelo.Libro;
+import modelo.LibroDAO;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Servlet implementation class BibliotecaControlador1
  */
-
+@WebServlet(urlPatterns = {"","/insertar"})
 public class BibliotecaControlador1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -29,7 +33,30 @@ public class BibliotecaControlador1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Servlet BibliotecaController");
 		RequestDispatcher despachador = null;
-		despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+		if (request.getServletPath().equals("")) {
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				ArrayList<Libro> libros;
+				libros = libroDAO.getLibros();
+				request.setAttribute("libros", libros);
+			} catch(RuntimeException e) {
+				request.setAttribute("error", e.getMessage());
+			}
+			despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+		} else if (request.getServletPath().equals("/insertar")) {
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				Libro libro = new Libro(Integer.parseInt(request.getParameter("isbn")),
+						request.getParameter("titulo"),request.getParameter("autor"));
+				request.setAttribute("info", "Libro " + libro + " añadido");
+				libroDAO.insertar(libro);
+			} catch(NumberFormatException e) {
+				request.setAttribute("error", e.getMessage());
+			} catch(RuntimeException e) {
+				request.setAttribute("error", e.getMessage());
+			}
+			despachador = request.getServletContext().getRequestDispatcher("/");
+		}
 		despachador.forward(request, response);
 	}
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,13 +1,57 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    
+ <%@ page import="java.util.ArrayList" %>
+ <%@ page import="modelo.Libro" %>   
+ 
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Mi primera chamba</title>
+<title>Biblioteca</title>
+<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-	<h1>Mi primera chamba web</h1>
-	<% out.println("Hola Mundo"); %>
+	<h1>Inserta un Libro</h1>
+	<% String error = (String)request.getAttribute("error");
+	if (error != null) { %>
+		<p> <%=error%></p>
+	<%}%>
+	<table  border="3">
+    <tr>
+        <th><b>ISBN</b></th>
+        <th><b>Título</b></th>
+        <th><b>Autor</b></th>
+    </tr>
+    <% String info = (String)request.getAttribute("info");
+    	if (info != null) { %>
+    		<p><%=info%></p>
+    <%}%>
+    
+    <% ArrayList<Libro> libros = (ArrayList<Libro>)request.getAttribute("libros");
+         if (libros != null) {
+        	 for(Libro l:libros) { %>
+        	 	<tr>
+        	 		<td><%=l.getIsbn()%></td>
+  					<td><%=l.getTitulo()%></td> 		
+  					<td><%=l.getAutor()%></td> 	
+         	<%}
+         }%>
+</table><br>
+
+    <div>
+        <form action="insertar" method="psot">
+            <label for="isbn">ISBN: </label>
+            <input type="text" name="isbn" id="isbn" placeholder="ISBN..."><br>
+
+            <label for="titulo">Título: </label>
+            <input type="text" name="titulo" id="titulo" placeholder="Titulo del libro..."><br>
+
+            <label for="autor">Autor: </label>
+            <input type="text" name="autor" id="autor" placeholder="Nombre del autor..."><br>
+
+            <input type="submit" value="Submit">
+        </form>
+    </div>
 </body>
 </html>
